d4234e51991867a896d29bde8acc7990c7c9ca18
Liam Hampton <liam.hampton@hotmail.co.uk>
2022-01-15 22:20:00 +0000

    feat: submission landing form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch(process.env.REACT_APP_FUNC_URL, {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+        }),
+      });
+      
+      if (res.status === 200) {
+        setName("");
+        setMessage("Name submitted successfully");
+      } else {
+        setMessage("An error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">Create</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
     </div>
   );
 }
