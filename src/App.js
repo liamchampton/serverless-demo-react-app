3c6ed39055295801f6dc7362192d75739f023375
Liam Hampton <liam.hampton@hotmail.co.uk>
2022-01-17 11:25:24 +0000

    add input fields and style the form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-// import logo from './logo.svg';
+import AzureFuncLogo from './azure-functions-logo.png';
+import BookEmoji from './book-emoji.png'
 import './App.css';
 
 function App() {
   const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = ("");
   const [message, setMessage] = useState("");
 
   let handleSubmit = async (e) => {
@@ -13,12 +16,16 @@ function App() {
         method: "POST",
         body: JSON.stringify({
           name: name,
+          price: price,
+          description: description,
         }),
       });
 
       if (res.status === 200) {
         setName("");
-        setMessage("Name submitted successfully");
+        setPrice("");
+        setDescription("");
+        setMessage("Item submitted successfully");
       } else {
         setMessage("An error occured");
       }
@@ -30,11 +37,29 @@ function App() {
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
+      <div className="iconDiv">
+        <img className="icon" src={AzureFuncLogo} alt="azure functions logo" />
+        <img className="icon" src={BookEmoji} alt="book emoji" />
+      </div>
+        <h1>Add an item to the catalogue</h1>
         <input
           type="text"
           value={name}
           placeholder="Name"
           onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          prefix='£'
+          value={price}
+          placeholder="Price"
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <input
+          type="textarea"
+          value={description}
+          placeholder="Description"
+          onChange={(e) => setDescription(e.target.value)}
         />
         <button type="submit">Create</button>
 
